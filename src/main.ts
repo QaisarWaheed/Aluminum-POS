2327eb38e4d7d66ba9b7f25029d48ef40ea83049
unknown <qaiserwaheed00@gmail.com>
2025-08-12 21:14:03 +0500

    error fixed again

--- src/main.ts
+++ src/main.ts
@@ -5,10 +5,11 @@ import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
-    origin: 'http://localhost:5173',
+    origin: true,
     methods: ['GET', 'POST', 'PUT', 'DELETE'],
     credentials: true,
   });
+
   const config = new DocumentBuilder()
     .setTitle('')
     .setDescription('')
