21dfbd2ac71ef76bfa564f116aa6bd325deef388
unknown <qaiserwaheed00@gmail.com>
2025-08-12 19:56:22 +0500

    error fixed

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,10 +2,22 @@ import { Module } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
 import { HardwareModule } from './features/hardware/harware.module';
 import { AluminumModule } from './features/aluminum/aluminum.module';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 
 @Module({
   imports: [
-    MongooseModule.forRoot('mongodb://localhost/hardware'),
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+
+    MongooseModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: (config: ConfigService) => ({
+        uri: config.get<string>('MONGO_URI', ''),
+      }),
+    }),
+
     AluminumModule,
     HardwareModule,
   ],
